1c548d6082a1f547a1c5f18befa25ba35be9f6bf
gmch1 <1731216262@qq.com>
2020-06-03 09:04:14 +0800

    fix

--- app/models/answer.js
+++ app/models/answer.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose
 const answerSchema = new Schema({
     __v: { type: String, select: false },
     content: { type: String, required: true },
-    answerer: { type: Schema.Types.ObjectId, ref: 'User', required: true, select: false },
+    answerer: { type: Schema.Types.ObjectId, ref: 'User', required: true},
     questionId: { type: String, required: true },
     voteCount: { type: Number, required: true, default: 0 }
 }, { timestamps: true })
